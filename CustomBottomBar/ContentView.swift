726591bf44aa719c1f5993e52a29d156c7ef2d80
Damilare <daremiarchs@gmail.com>
2025-11-09 13:47:01 +0100

    Initial Commit

---
+++ CustomBottomBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomBottomBar
+//
+//  Created by Damilare Adekunle on 09/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomBottomBar/CustomBottomBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBottomBarApp.swift
+//  CustomBottomBar
+//
+//  Created by Damilare Adekunle on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomBottomBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
