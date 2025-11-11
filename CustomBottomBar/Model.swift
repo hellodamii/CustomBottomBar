0d507eefc8edc4c322bc52ae8bc9ec3c035d6ec4
Damilare <daremiarchs@gmail.com>
2025-11-11 03:06:37 +0100

    dependencies

---
+++ CustomBottomBar/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  CustomBottomBar
+//
+//  Created by Damilare Adekunle on 11/11/2025.
+//
+
+import SwiftUI
+
+struct Model: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Model()
+}
