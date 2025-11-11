8207032a84f25a76b827c99d8df856bae51e8f87
Damilare <daremiarchs@gmail.com>
2025-11-11 01:03:28 +0100

    practice

---
+++ CustomBottomBar/Helpers/AnimatedBottomBar.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedBottomBar.swift
+//  CustomBottomBar
+//
+//  Created by Damilare Adekunle on 09/11/2025.
+//
+
+import SwiftUI
+
+struct AnimatedBottomBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedBottomBar()
+}
